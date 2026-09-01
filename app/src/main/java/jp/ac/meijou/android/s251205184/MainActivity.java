ad99e32e8171e85caa5fe585b4fc2102ea3c09aa
Shuya Muramatsu <237186723+Muramatsu-Shuya@users.noreply.github.com>
2026-09-01 15:56:08 +0900

    2日目 a,b,c,それ以外 各場合によって表示される結果が異なるように修正

--- app/src/main/java/jp/ac/meijou/android/s251205184/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s251205184/MainActivity.java
@@ -3,6 +3,8 @@ package jp.ac.meijou.android.s251205184;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -30,24 +32,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        prefDataStore = PrefDataStore.getInstance((this));
-        prefDataStore.getString("text")
-                .ifPresent(text -> binding.textView.setText(text));
+        prefDataStore = PrefDataStore.getInstance(this);
+        prefDataStore.getString("name")
+                .ifPresent(name -> {
+                    if("a".equals(name)){
+                        binding.textView.setText("Aの画像");
+                        binding.imageView.setImageResource(R.drawable.outline_approval_delegation_24);
+                    } else if ("b".equals(name)) {
+                        binding.textView.setText("Bの画像");
+                        binding.imageView.setImageResource(R.drawable.outline_approval_delegation_25);
+                    } else if ("c".equals(name)) {
+                        binding.textView.setText("Cの画像");
+                        binding.imageView.setImageResource(R.drawable.outline_approval_delegation_26);
+                    }else{
+                        binding.textView.setText("知らない画像");
+                        binding.imageView.setImageResource(R.drawable.baseline_block_24);
+                    }
+                });
 
-        binding.textView.setText("-");
 
-        binding.imageView.setImageResource(R.drawable.baseline_block_24);
+        //binding.textView.setText("-");
+
+        //binding.imageView.setImageResource(R.drawable.baseline_block_24);
 
         binding.button.setOnClickListener(view ->{
             var text = binding.editText.getText().toString();
             binding.textView.setText(text);
-            binding.imageView.setImageResource(R.drawable.outline_approval_delegation_24);
-        });
-
-        binding.SaveButton.setOnClickListener(view -> {
-            var text = binding.editText.getText().toString();
-            prefDataStore.setString("text", text);
-            binding.imageView.setImageResource(R.drawable.outline_approval_delegation_24);
+            binding.imageView.setImageResource(R.drawable.outline_approval_delegation_25);
         });
 
         binding.editText.addTextChangedListener(new TextWatcher() {
@@ -68,6 +79,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.SaveButton.setOnClickListener(view -> {
+            var text = binding.editText.getText().toString();
+            if("a".equals(text)){
+                binding.imageView.setImageResource(R.drawable.outline_approval_delegation_24);
+            }else if("b".equals(text)){
+                binding.imageView.setImageResource(R.drawable.outline_approval_delegation_25);
+            }else if("c".equals(text)) {
+                binding.imageView.setImageResource(R.drawable.outline_approval_delegation_26);
+            }else{
+                binding.imageView.setImageResource(R.drawable.baseline_block_24);
+            }
+            prefDataStore.setString("name", text);
+            //binding.imageView.setImageResource(R.drawable.outline_approval_delegation_24);
+        });
+
+        binding.DeleteButton.setOnClickListener(view -> {
+            String text = null;
+            binding.textView.setText(text);
+            prefDataStore.setString("name", text);
+        });
 
     }
 }
