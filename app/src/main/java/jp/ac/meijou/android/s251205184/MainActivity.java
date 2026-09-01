04599fb312242fa9d22fe10944499abeebacded8
Shuya Muramatsu <237186723+Muramatsu-Shuya@users.noreply.github.com>
2026-09-01 12:17:36 +0900

    2日目 中間コミット

--- app/src/main/java/jp/ac/meijou/android/s251205184/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s251205184/MainActivity.java
@@ -1,6 +1,7 @@
 package jp.ac.meijou.android.s251205184;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s251205184.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.text.setText(R.string.pay);
+
+        binding.imageView.setImageResource(R.drawable.baseline_block_24);
     }
 }
