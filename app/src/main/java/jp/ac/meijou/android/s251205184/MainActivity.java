ea20c7290d233f7fc0f8fd0ff3ea4ee945e34f55
Shuya Muramatsu <237186723+Muramatsu-Shuya@users.noreply.github.com>
2026-09-01 14:31:17 +0900

    2日目 データストア コミット

--- app/src/main/java/jp/ac/meijou/android/s251205184/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s251205184/MainActivity.java
@@ -1,6 +1,8 @@
 package jp.ac.meijou.android.s251205184;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +16,7 @@ import jp.ac.meijou.android.s251205184.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +30,44 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        prefDataStore = PrefDataStore.getInstance((this));
+        prefDataStore.getString("text")
+                .ifPresent(text -> binding.textView.setText(text));
 
-        binding.text.setText(R.string.pay);
+        binding.textView.setText("-");
 
         binding.imageView.setImageResource(R.drawable.baseline_block_24);
+
+        binding.button.setOnClickListener(view ->{
+            var text = binding.editText.getText().toString();
+            binding.textView.setText(text);
+            binding.imageView.setImageResource(R.drawable.outline_approval_delegation_24);
+        });
+
+        binding.SaveButton.setOnClickListener(view -> {
+            var text = binding.editText.getText().toString();
+            prefDataStore.setString("text", text);
+            binding.imageView.setImageResource(R.drawable.outline_approval_delegation_24);
+        });
+
+        binding.editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = s.toString();
+                binding.textView.setText(text);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+        });
+
+
     }
 }
