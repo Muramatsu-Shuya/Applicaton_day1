51bdea18109ec680f1fa68dce58b9b33845c0f35
Shuya Muramatsu <237186723+Muramatsu-Shuya@users.noreply.github.com>
2026-09-02 15:00:11 +0900

    画面遷移 明示的Intent 暗黙的Intent

--- app/src/main/java/jp/ac/meijou/android/s251205184/MainActivity3.java
+++ app/src/main/java/jp/ac/meijou/android/s251205184/MainActivity3.java
@@ -1,6 +1,8 @@
 package jp.ac.meijou.android.s251205184;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s251205184.databinding.ActivityMain3Binding;
+import jp.ac.meijou.android.s251205184.databinding.ActivityMainBinding;
+
 public class MainActivity3 extends AppCompatActivity {
+
+    private ActivityMain3Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main3);
+        binding = ActivityMain3Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Intent intent = getIntent();
+        String setText = intent.getStringExtra("editText");
+        binding.result.setText(setText);
+
+        binding.button0.setOnClickListener(view -> {
+            binding.result.setText(binding.result.getText().toString() + "0");
         });
     }
 }
